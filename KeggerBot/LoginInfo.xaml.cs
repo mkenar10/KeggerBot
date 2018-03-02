c75db8059da76859f754d403a1624d81e1360d89
mkenar <mkenar10@gmail.com>
2018-03-02 10:39:13 -0600

    Updated LoginInfo to display what the user typed in.

--- KeggerBot/LoginInfo.xaml.cs
+++ KeggerBot/LoginInfo.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Net.Http;
 using Xamarin.Forms;
 
 namespace KeggerBot
@@ -12,6 +12,21 @@ namespace KeggerBot
             InitializeComponent();
 
             logo.Source = ImageSource.FromFile("SogetiKegBot.Droid/Resources/drawable/beer1.png");
+        }
+
+        void Login_Clicked(object sender, System.EventArgs e)
+        {
+            var user = username.Text.ToString();
+            var pass = password.Text.ToString();
+            sample.Text = user + " " + pass;
+
+           // HttpClient httpClient = new HttpClient();
+            //var response = async httpClient.GetAsync(new Uri("HTTP://KEGGERBOTAPI.AZUREWEBSITES.NET/API"));
+
+
+
+
+            //go to main page
         }
     }
 }
